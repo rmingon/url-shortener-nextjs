f4fe51daed24c9c566d300e0415841b0e75ba81b
Ronan <ronanmingon69550@gmail.com>
2022-12-02 06:04:31 +0100

    add footer section

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="px-12  h-24 bg-violet-900">
+      <div className="border-t border-gray-400 pt-12">
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  )
+}
