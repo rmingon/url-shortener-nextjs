bccce7f2e783422169ab1fe6cf085b7f4500eef3
Ronan <ronanmingon69550@gmail.com>
2022-12-03 11:03:57 +0100

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,19 @@
 export default function Footer() {
   return (
-    <div className="px-12  h-24 bg-violet-900">
-      <div className="border-t border-gray-400 pt-12">
-        <div className="">
-
+    <div className="px-12 h-20 bg-violet-900">
+      <div className="border-t border-gray-400 pt-6">
+        <div className="flex justify-center items-center">
+          <ul className="flex text-gray-200 space-x-3">
+            <li className="cursor-pointer">FAQ</li>
+            <li className="cursor-pointer">Pricing</li>
+            <li className="cursor-pointer">Blog</li>
+            <li className="cursor-pointer">Trems & Policy</li>
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Sitemap</li>
+          </ul>
+          <div className="">
+            YO
+          </div>
         </div>
       </div>
     </div>
