00d2ea9b11fc02a9bbdd96f8bfd5b72b2ed76e7b
Ronan <ronanmingon69550@gmail.com>
2022-12-07 06:32:44 +0100

    add request login token

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import jwt from 'jsonwebtoken';
 
 import bdd from './bdd'
 
@@ -13,8 +14,9 @@ export default async function login (
 ) {
   const {email, password} : Data = req.body
   try {
-    const login = await bdd.logIn(email, password)
-    res.status(200).json(login)
+    const {ok, ...rest} = await bdd.logIn(email, password)
+    const token = jwt.sign(rest, '6811058d-7f62-481b-89bd-d43fa3861429');
+    res.status(200).json({...rest, token})
   } catch (e) {
     res.status(401).json(e)
   }
