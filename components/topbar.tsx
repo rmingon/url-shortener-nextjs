18f2650e47fe676ed79de6e20d8412da6251f442
Ronan <ronanmingon69550@gmail.com>
2022-12-21 06:52:47 +0100

    add hook useSessionStorage

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -3,8 +3,13 @@ import Modal from "./modal";
 import SignIn from "./sign-in";
 import SignUp from "./sign-up";
 import {useState} from "react";
+import {toast} from "./toast";
 
 export default function TopBar () {
+
+  const click = () => {
+    toast.notify("COUCOU", {duration: 2 , type: "warning"})
+  }
 
   const [showModalSignIn, setShowModalSignIn] = useState(false);
   const [showModalSignUp, setShowModalSignUp] = useState(false);
@@ -13,7 +18,7 @@ export default function TopBar () {
     <header className='flex space-x-3 items-center mt-5 justify-between px-5'>
       <Image src={'/vercel.svg'} width={'100'} height={'100'} alt={''} className={'mr-10'} />
       <ul className="flex space-x-3">
-        <li className="cursor-pointer">Features</li>
+        <li className="cursor-pointer" onClick={() => click()}>Features</li>
         <li className="cursor-pointer">Developers</li>
         <li className="cursor-pointer">Pricing</li>
         <li className="cursor-pointer">About</li>
@@ -34,7 +39,7 @@ export default function TopBar () {
       >
         <SignUp></SignUp>
       </Modal>
-      <ul className="flex space-x-3 mr-2 items-center ">
+      <ul className="flex space-x-3 mr-2 items-center">
         <li onClick={() => setShowModalSignIn(true)} className="cursor-pointer px-3 py-2 bg-purple-300 rounded-xl hover:bg-purple-500 active:bg-purple-200 transition duration-100">Sign in</li>
         <li onClick={() => setShowModalSignUp(true)} className="cursor-pointer px-3 py-2 bg-gray-100 rounded-xl hover:bg-gray-200 transition duration-100">Sign up</li>
       </ul>
