966214f53948e2a3a2499e7dd740a3c8082ab49c
Ronan <ronanmingon69550@gmail.com>
2022-12-21 06:53:30 +0100

    add provider in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
 import '../styles/magic.min.css'
 import type { AppProps } from 'next/app'
+import {createContext, useEffect, useRef, useState} from "react";
+import jwt_decode from "jwt-decode";
+import {useLocalStorage} from "../hooks/useSessionStorage";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps}/>
+  const [isUser, setIsUser] = useState(false);
+  const [user, setUser] = useState({});
+
+  const [token, setToken] = useLocalStorage('token', "")
+
+  useEffect(() => {
+    console.log(token)
+    if (token) {
+      const user : {} = jwt_decode(token);
+      setUser(user);
+      setIsUser(true);
+    } else {
+      setIsUser(false)
+    }
+  })
+
+  const User = createContext({...user, isUser});
+
+  return (
+    <User.Provider value={user}>
+      <Component {...pageProps}/>
+    </User.Provider>
+  );
 }
