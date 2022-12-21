a364ca8090f30b4dda8c9b9c33da80c77fd23749
Ronan <ronanmingon69550@gmail.com>
2022-12-21 06:53:03 +0100

    add metadata in account

--- pages/api/registration.ts
+++ pages/api/registration.ts
@@ -5,15 +5,16 @@ import bdd from './bdd'
 interface Data {
   email: string;
   password: string;
+  meta: {pseudo: string}
 }
 
 export default async function registration (
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {email, password} : Data = req.body
+  const {email, password, meta} : Data = req.body
   try {
-    const login = await bdd.signUp(email, password)
+    const login = await bdd.signUp(email, password, {metadata: meta})
     res.status(200).json(login)
   } catch (e) {
     res.status(401).json(e)
