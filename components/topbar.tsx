795bc26c733267f89d0ac71ebeceb8a6aa1b30f0
Ronan <ronanmingon69550@gmail.com>
2022-12-21 21:24:53 +0100

    add connexion

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -4,21 +4,18 @@ import SignIn from "./sign-in";
 import SignUp from "./sign-up";
 import {useState} from "react";
 import {toast} from "./toast";
+import {useUser} from "../context/user-context";
 
 export default function TopBar () {
 
-  const click = () => {
-    toast.notify("COUCOU", {duration: 2 , type: "warning"})
-  }
-
   const [showModalSignIn, setShowModalSignIn] = useState(false);
   const [showModalSignUp, setShowModalSignUp] = useState(false);
-
+  const { user, setUser } = useUser();
   return (
     <header className='flex space-x-3 items-center mt-5 justify-between px-5'>
       <Image src={'/vercel.svg'} width={'100'} height={'100'} alt={''} className={'mr-10'} />
       <ul className="flex space-x-3">
-        <li className="cursor-pointer" onClick={() => click()}>Features</li>
+        <li className="cursor-pointer">Features</li>
         <li className="cursor-pointer">Developers</li>
         <li className="cursor-pointer">Pricing</li>
         <li className="cursor-pointer">About</li>
@@ -29,7 +26,7 @@ export default function TopBar () {
         height="w-auto"
         width="h-auto"
       >
-        <SignIn></SignIn>
+        <SignIn setShowModal={setShowModalSignIn}></SignIn>
       </Modal>
       <Modal
         onClose={() => setShowModalSignUp(false)}
@@ -39,10 +36,17 @@ export default function TopBar () {
       >
         <SignUp></SignUp>
       </Modal>
-      <ul className="flex space-x-3 mr-2 items-center">
-        <li onClick={() => setShowModalSignIn(true)} className="cursor-pointer px-3 py-2 bg-purple-300 rounded-xl hover:bg-purple-500 active:bg-purple-200 transition duration-100">Sign in</li>
-        <li onClick={() => setShowModalSignUp(true)} className="cursor-pointer px-3 py-2 bg-gray-100 rounded-xl hover:bg-gray-200 transition duration-100">Sign up</li>
-      </ul>
+      {user.name ? (
+        <ul className="flex space-x-3 mr-2 items-center">
+          <li onClick={() => setShowModalSignIn(true)} className="cursor-pointer px-3 py-2 bg-purple-300 rounded-xl hover:bg-purple-500 active:bg-purple-200 transition duration-100">Profile</li>
+          <li onClick={() => setShowModalSignUp(true)} className="cursor-pointer px-3 py-2 bg-gray-100 rounded-xl hover:bg-gray-200 transition duration-100">Log out</li>
+        </ul>
+      ): (
+        <ul className="flex space-x-3 mr-2 items-center">
+          <li onClick={() => setShowModalSignIn(true)} className="cursor-pointer px-3 py-2 bg-purple-300 rounded-xl hover:bg-purple-500 active:bg-purple-200 transition duration-100">Sign in</li>
+          <li onClick={() => setShowModalSignUp(true)} className="cursor-pointer px-3 py-2 bg-gray-100 rounded-xl hover:bg-gray-200 transition duration-100">Sign up</li>
+        </ul>
+      )}
     </header>
   )
 }
