37462c34a2354a2bbc3f65908db6877dce0eb322
rmingon <rmingon@doing.fr>
2023-01-02 07:16:21 +0100

    add profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,15 @@
+import Sidebar from "../components/sidebar";
+
 export default function Profile () {
   return (
-    <div>
-      PROFILE
-    </div>
+      <div className={"flex h-screen"}>
+        <div className={"w-2/12"}>
+          <Sidebar></Sidebar>
+        </div>
+        <div className={""}>
+          YOYOYO
+        </div>
+      </div>
+
   )
 }
