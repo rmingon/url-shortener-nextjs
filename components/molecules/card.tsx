861d22a6542c4116d8951f192ffb72c590d495ff
rmingon <rmingon@doing.fr>
2023-01-05 08:18:09 +0100

    move card

--- components/molecules/card.tsx
+++ components/molecules/card.tsx
@@ -1,14 +1,16 @@
+import {ReactNode} from "react";
+
 interface props {
-  svg: string,
+  children: ReactNode,
   title: string,
   comment: string,
   count: number
 }
 
-export default function Card ({svg, title, comment, count}: props) {
+export default function Card ({children, title, comment, count}: props) {
   return (
     <div className="text-center cursor-pointer rounded-2xl hover:shadow-2xl duration-200 transition px-2 flex flex-col items-center">
-      <div className="mt-4" dangerouslySetInnerHTML={{__html: svg}}></div>
+      <div className="mt-4">{children}</div>
       <span className="font-roboto-bold text-purple-800">{title}</span>
       <p className="mt-3">{comment}</p>
       <p className="font-roboto-bold text-gray-200 my-2">0{count}</p>
